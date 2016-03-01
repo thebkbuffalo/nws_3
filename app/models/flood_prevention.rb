ede3bfd880aa807b3763b5de5f52014eb8e5dbf7
Evan Berg <evan.m.berg@gmail.com>
2016-03-01 13:54:51 -0800

    added https option to flood prevention model

--- app/models/flood_prevention.rb
+++ app/models/flood_prevention.rb
@@ -1,4 +1,4 @@
 class FloodPrevention < ActiveRecord::Base
-  has_attached_file :fp_pic, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://www.fillmurray.com/300/300"
+  has_attached_file :fp_pic, :s3_protocol => :https, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://www.fillmurray.com/300/300"
   validates_attachment_content_type :fp_pic, content_type: /\Aimage\/.*\Z/
 end
