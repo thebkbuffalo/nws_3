21176c63247f614a01de89ba2b0617b8a0d48492
Evan Berg <evan.m.berg@gmail.com>
2016-03-02 12:38:03 -0800

    added the s3 protocol to https line into paperclip defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
       :bucket => 'nwstwo',
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_protocol => 'https'
   }
 end
