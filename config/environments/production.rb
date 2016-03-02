a8702e687513600c9f93d3cffd13ad5b8d0c2448
Evan Berg <evan.m.berg@gmail.com>
2016-03-02 13:47:04 -0800

    added region to paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     # :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :region => 'us-west-2',
     :s3_protocol => :https,
     :s3_credentials => {
       :bucket => 'nationalwaterservice',
