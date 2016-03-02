b243c548e4352fa03f4e0cf2ad394a2b181b702e
Evan Berg <evan.m.berg@gmail.com>
2016-03-02 13:52:08 -0800

    changed region to us west 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     # :s3_host_name => 's3-us-west-2.amazonaws.com',
-    :region => 'us-west-2',
+    :region => 'us-west-1',
     :s3_protocol => :https,
     :s3_credentials => {
       :bucket => 'nationalwaterservice',
