128069a64621fbe9bc6783f7a9dcf3bdd023d348
Evan Berg <evan.m.berg@gmail.com>
2016-03-08 12:35:34 -0800

    added video things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :videos
   root 'welcome#index'
 
   resources :water_testing_services
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301074922) do
+ActiveRecord::Schema.define(version: 20160308203154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -335,6 +335,11 @@ ActiveRecord::Schema.define(version: 20160301074922) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "water_conditioners", force: :cascade do |t|
